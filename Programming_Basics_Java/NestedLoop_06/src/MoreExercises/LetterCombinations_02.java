0078104028b44b896fe2b5dfe66f1968546c92ee
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-01-05 10:52:05 +0200

    Add solutions

---
+++ Programming_Basics_Java/NestedLoop_06/src/MoreExercises/LetterCombinations_02.java
@@ -0,0 +1,40 @@
+package MoreExercises;
+
+import java.util.Scanner;
+
+public class LetterCombinations_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstLetter = scanner.nextLine();
+        String lastLetter = scanner.nextLine();
+        String missLetter = scanner.nextLine();
+
+        char first = firstLetter.charAt(0);
+        char last = lastLetter.charAt(0);
+        char miss = missLetter.charAt(0);
+
+        int count = 0;
+
+        for (char i = first; i <= last; i++) {
+            if (i == miss) {
+                continue;
+            }
+            for (char j = first; j <= last; j++) {
+                if (j == miss) {
+                    continue;
+                }
+                for (char k = first; k <= last; k++) {
+                    if (k == miss) {
+                        continue;
+                    }
+                    count++;
+                    System.out.printf("%s%s%s ", i, j, k);
+
+                }
+            }
+        }
+        System.out.print(count);
+    }
+}
+
