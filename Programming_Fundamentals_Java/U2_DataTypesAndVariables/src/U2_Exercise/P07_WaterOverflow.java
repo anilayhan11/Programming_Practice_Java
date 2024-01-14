118fd395e7443d30c2d2858c7e2ed3143b9726b6
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-01-14 16:44:56 +0300

    Add solutions

---
+++ Programming_Fundamentals_Java/U2_DataTypesAndVariables/src/U2_Exercise/P07_WaterOverflow.java
@@ -0,0 +1,25 @@
+package U2_Exercise;
+
+import java.util.Scanner;
+
+public class P07_WaterOverflow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inputCount = Integer.parseInt(scanner.nextLine());
+        int currentLiters = 0;
+        int capacity = 255;
+
+        for (int i = 0; i <= inputCount - 1; i++) {
+            int waterLiters = Integer.parseInt(scanner.nextLine());
+
+            if (capacity - waterLiters >= 0) {
+                currentLiters += waterLiters;
+                capacity -= waterLiters;
+            } else {
+                System.out.println("Insufficient capacity!");
+            }
+        }
+        System.out.printf("%d", currentLiters);
+    }
+}
