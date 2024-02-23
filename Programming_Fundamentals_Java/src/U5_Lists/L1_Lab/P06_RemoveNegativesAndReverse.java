6267fcbfb08cd0d43ef5bfbae3bac208e51aab4d
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-02-23 20:01:50 +0300

    Add solutions

---
+++ Programming_Fundamentals_Java/src/U5_Lists/L1_Lab/P06_RemoveNegativesAndReverse.java
@@ -0,0 +1,27 @@
+package U5_Lists.L1_Lab;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P06_RemoveNegativesAndReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbersList = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        numbersList.removeIf(e -> e < 0);
+        Collections.reverse(numbersList);
+
+        if (numbersList.isEmpty()) {
+            System.out.println("empty");
+        } else {
+            System.out.println(numbersList.toString().replaceAll("[\\[\\],]", ""));
+        }
+    }
+}
